1b8415fc833fcefe5c419e727c15ee27be542423
Robert Schonberger <rschonberger@gmail.com>
2009-11-17 12:57:31 +1100

    add a main package, for the fun of it.

---
+++ securerandom/filemain.go
@@ -0,0 +1,19 @@
+package main
+// Author rschonberger@gomail.com (Robert Schonberger) 
+// Released under the Creative Commons - Attribution, Non commercial Usage OK. 
+// http://creativecommons.org/licenses/by-nc-nd/3.0 2009
+
+import "./filesource"
+import "fmt"
+import "rand"
+
+func main() {
+       src, _ := filesource.NewFileSeededSource("/dev/random");
+       rgn := rand.New(src);
+       var sum int64 = 0;
+       for i:= 0; i < 5000000; i++ {
+               sum += int64(rgn.Int31());
+       }
+       fmt.Printf("Hello, bitches!%f \n", sum / 5000000);
+}
+
